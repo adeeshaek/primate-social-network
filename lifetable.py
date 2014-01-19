a86a03fdf0f36d86c64147b43d8cd5dc6fb43e33
Adeesha Ekanayake <adeeshaekanayake@gmail.com>
2014-01-18 21:28:04 -0500

    made alterations to lifetable

--- lifetable.py
+++ lifetable.py
@@ -13,7 +13,7 @@ class LifeTable:
 	male_life_table = {}
 	female_life_table = {}
 
-	def chance_of_death(self, females_to_male, agent):
+	def chance_of_death(self, females_to_male, age, sex):
 		"""
 		returns the probability of an individual dying this year.
 		chance_of_death = chance_of_death_by_age * chance_of_death_by_proportion
@@ -25,12 +25,17 @@ class LifeTable:
 			chance_of_death_by_proportion = 70 + (10 * females_to_male)
 
 		chance_of_death_by_proportion is always > 0
+
+		parameters
+		----------
+		females_to_male: the number of females to one male
+		age: age in years as integer
+		sex: SEX_FEMALE or SEX_MALE
 		"""
-		age = agent.age
 		chance_of_death_by_age = 0
 		chance_of_death_by_proportion = 0
 
-		if (agent.sex = "SEX_FEMALE"):
+		if (sex == "SEX_FEMALE"):
 			chance_of_death_by_age = self.female_life_table[age][0]
 			chance_of_death_by_proportion = 120 - (10 * females_to_male)
 
@@ -42,14 +47,14 @@ class LifeTable:
 
 		return chance_of_death
 
-	def chance_of_birth(self, females_to_male, agent):
+	def chance_of_birth(self, females_to_male, age):
 		"""
 		returns the probability that a female will give birth this year. 
 		chance_of_birth = chance_of_birth_by_age * chance_of_birth_by_proportion
 
 		chance_of_birth_by_proportion = 120 - (10 * females_to_male)
 		"""
-		chance_of_birth_by_age = self.female_life_table[agent.age][1]
+		chance_of_birth_by_age = self.female_life_table[age][1]
 
 		chance_of_birth_by_proportion = 120 - (10 * females_to_male)
 
