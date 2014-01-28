67767c529e701cf5f3146f06b177550a3e05c6db
Adeesha Ekanayake <adeeshaekanayake@gmail.com>
2014-01-27 20:47:40 -0500

    writing main run loop

---
+++ main.py
@@ -0,0 +1,21 @@
+"""	
+Macaque Simulation Project
+Adeesha Ekanayake 
+5/12/2013
+
+main.py
+-------
+Main run loop
+"""
+
+from agent import AgentClass
+from group import AgentGroup
+from lifetable import LifeTable
+from seed import SeedGenerator
+
+def main():
+	print "main"
+
+
+if __name__ == '__main__':
+	main()
