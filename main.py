fbab83c573b958ed99c69014a1416bc045356fff
Adeesha Ekanayake <adeeshaekanayake@gmail.com>
2014-01-31 09:44:41 -0500

    in main run loop, decomposed check_for_birth and check_for death into functions

--- main.py
+++ main.py
@@ -45,22 +45,15 @@ def main():
 			new_generation.promote_agent(new_agent)
 
 			#check for birth
-			if (agent_index in this_generation.female_set):
-				chance_of_giving_birth =\
-				 lifetable.chance_of_birth(
-				 	females_to_male, this_agent.age)
-
-				#do a die roll
-				if (random_module.roll(chance_of_giving_birth)):
-					new_generation.give_birth_to_agent(
-						new_agent, random_module, new_generation)
+			check_for_birth(this_generation, new_generation,
+				this_agent, new_agent, females_to_male,
+				agent_index, lifetable, random_module)
 
 			#check for death
-			chance_of_death = lifetable.chance_of_death(
-				females_to_male, this_agent.age, this_agent.sex)
-
-			if (random_module.roll(chance_of_death)):
-				new_generation.mark_agent_as_dead(new_agent)
+			check_for_death(lifetable, females_to_male, 
+				this_agent, new_agent, new_generation,
+				random_module)
+			
 			#check for dispersal
 
 		#set the old gen to the new one
@@ -71,6 +64,56 @@ def main():
 			#print the new generation
 			for agent_index in new_generation.whole_set:
 				print this_generation.agent_array[agent_index]
+
+def check_for_death(lifetable, females_to_male, this_agent,
+	new_agent, new_generation, random_module):
+	"""
+	checks if an agent should die by getting the probability
+	from the lifetable, then performing a dieroll for that
+	probability. If true is returned, the agent in the 
+	new_generation is marked as being dead
+	"""
+	chance_of_death = lifetable.chance_of_death(
+		females_to_male, this_agent.age, this_agent.sex)
+
+	if (random_module.roll(chance_of_death)):
+		new_generation.mark_agent_as_dead(new_agent)
+
+def check_for_birth(
+	this_generation, new_generation, this_agent, new_agent,
+	females_to_male, agent_index, lifetable, random_module):
+	"""
+	checks if an agent is about to give birth, by getting the
+	probability of giving birth from the lifetable. If so, performs
+	a die roll. If die roll returns true, then the newborn is added
+	to the group
+
+	parameters
+	----------
+	this_generation:
+	new_generation:
+	this_agent:
+	new_agent:
+	females_to_male:
+	agent_index:
+	lifetable:
+	random_module:
+
+	properties modified
+	-------------------
+	new_generation: if newborn added
+	new_agent: marked as parent if newborn added 
+	"""
+	#check for birth
+	if (agent_index in this_generation.female_set):
+		chance_of_giving_birth =\
+		 lifetable.chance_of_birth(
+		 	females_to_male, this_agent.age)
+
+		#do a die roll
+		if (random_module.roll(chance_of_giving_birth)):
+			new_generation.give_birth_to_agent(
+				new_agent, random_module, new_generation)
 
 
 if __name__ == '__main__':
